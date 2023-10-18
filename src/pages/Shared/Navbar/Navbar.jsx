64e38cb5d93ae58b0294577266e5371aadd3c9ac
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2023-10-18 23:58:45 +0600

    navbar fixed

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,49 +1,90 @@
 
-const Navbar = () => {
 
+import { useState } from "react";
+
+
+
+import logo from '../../../assets/logo.png';
+import cartIcon from '../../../assets/icon/cart-icon.png';
+import avatar from '../../../assets/icon/avatar.svg';
+
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const navOptions = <>
+        <a href="#" className="block text-white py-2 uppercase" >
+            Home
+        </a >
+        <a href="#" className="block text-white py-2 uppercase">
+            Contact us
+        </a>
+        <a href="#" className="block text-white py-2 uppercase">
+            Dashboard
+        </a>
+        <a href="#" className="block text-white py-2 uppercase">
+            Our Menu
+        </a>
+        <a href="#" className="block text-white py-2 uppercase">
+            Our Shop
+        </a>
+        <a href="#" className="flex items-center">
+            <img width="40px" src={cartIcon} alt="" />
+        </a>
+        <a href="#" className="flex  items-center text-white py-2 uppercase">
+            <p>Sign out</p>
+            <img width="40px" src={avatar} alt="" />
+        </a>
+    </>
     return (
-        <>
-            <div className="max-w-screen-xl navbar fixed bg-opacity-30 z-10 bg-black text-white">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
+        <nav className="max-w-screen-xl w-full px-10 fixed bg-opacity-40 z-10 bg-black text-white">
+            <div className="mx-auto">
+                <div className="flex items-center justify-between">
+                    <div className="text-white font-bold text-center">
+                        <img width="40px" className="mx-auto" src={logo} alt="" />
+                        <h3>Bistro Boss</h3>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <div
+                        className={`hidden md:flex items-center space-x-4 transition-opacity duration-300 ${isMenuOpen ? 'opacity-0' : 'opacity-100'
+                            }`}
+                    >
+                        {navOptions}
+                    </div>
+                    <div className="md:hidden">
+                        {/* Mobile Menu Button */}
+                        <button className="text-white" onClick={toggleMenu}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="w-6 h-6"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M4 6h16M4 12h16m-7 6h7"></path>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+                {/* Mobile Menu */}
+                {isMenuOpen && (
+                    <div
+                        className={`md:hidden mt-2 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0'
+                            }`}
+                    >
+                        {navOptions}
+                    </div>
+                )}
             </div>
-        </>
+        </nav>
     );
 };
 
 export default Navbar;
+
