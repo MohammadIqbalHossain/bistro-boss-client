2de6819cfae82fbf0871f74379c24021071fe347
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2023-10-26 02:54:19 +0600

    login and context added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,13 @@
 import {
     createBrowserRouter,
-    
+
 } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/Signup/SignUp";
 
 export const router = createBrowserRouter([
     {
@@ -18,17 +19,21 @@ export const router = createBrowserRouter([
                 element: <Home></Home>
             },
             {
-                 path: 'menu',
-                 element: <Menu></Menu>
+                path: 'menu',
+                element: <Menu></Menu>
             },
             {
                 path: 'order/:category',
                 element: <Order></Order>
-           },
-           {
+            },
+            {
                 path: 'login',
                 element: <Login></Login>
-           },
+            },
+            {
+                path: 'signup',
+                element: <SignUp></SignUp>
+            },
         ]
     },
 ]);
